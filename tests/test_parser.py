6dadfbdf5aae0ac6b37a9d604c38816b256f54e7
Avery Faller <avyk37@gmail.com>
2016-03-21 12:00:57 -0400

    More Tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -3,6 +3,9 @@
 
 import pytest
 from stupidlang.parser import typer
+from stupidlang.parser import lex
+from stupidlang.parser import syn
+from stupidlang.parser import parse
 
 def test_typer():
     assert typer('true') == True
@@ -12,14 +15,17 @@ def test_typer():
     assert typer('.1.1') == '.1.1'
     
 def test_lex():
-    assert lex('()') = [(, )]
+    assert lex('()') == ['(', ')']
     
 def test_syn():
-    assert syn([(, )]) = []
+    assert syn(['(', ')']) == []
+    assert syn(['(', '+', 8, 2, ')']) == ['+', 8, 2]
     
 def test_parse():
-    assert parse('()') = []
-    assert parse('(())') = [[]]
+    assert parse('()') == []
+    assert parse('(+ 1 2)') == ['+', 1, 2]
+    assert parse('(())') == [[]]
+    assert parse('(+ (+ 1 1) 2)') == ['+', ['+', 1, 1], 2]
     
 print('Running test_parser.py')
 print('Testing typer')
